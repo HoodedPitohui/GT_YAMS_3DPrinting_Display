e5282a3fbc3b46ed82c894adabadf11a9abf26e5
Karthik Shaji <58968966+HoodedPitohui@users.noreply.github.com>
2022-07-08 00:14:17 -0400

    The day's javascript work

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-const http = require('http');
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.json());
-app.use(express.static("express"));
-// default URL for website
-app.use('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/index.html'));
-    //__dirname : It will resolve to your project folder.
-  });
-const server = http.createServer(app);
-const port = 3000;
-server.listen(port);
-console.debug('Server listening on port ' + port)
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+app.listen(5656, () => {
+    console.log('http://localhost:5656')
+})
