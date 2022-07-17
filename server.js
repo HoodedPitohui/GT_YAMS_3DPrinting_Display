718669a5b387b616cbaacd96fb235414043e3747
Karthik Shaji <58968966+HoodedPitohui@users.noreply.github.com>
2022-07-17 09:09:32 -0400

    Rebuilt page in pug

--- server.js
+++ server.js
@@ -6,29 +6,8 @@ app.set('view engine', 'pug');
 app.get('/', (req, res) => {
     res.render('index')
 })
-app.get('/charles', (req, res) => {
-  res.render('index', {
-    firstname: 'Charles',
-    lastname: 'Chaplin',
-    tag: 'The Little Tramp',
-    content: '...',
-    movies: 'hi'
-  })
-})
-app.get('/marilyn', (req, res) => {
-  res.render('index', {
-    firstname: 'Marilyn',
-    lastname: 'Monroe',
-    tag: 'Being normal is boring',
-    content: '...',
-    movies: 'hi'
-  })
-})
-app.get('/jean', (req, res) => {
-  res.render('index')
-})
 app.get('/', (req, res) => {
-  res.redirect('/charles')
+  res.redirect('index')
 })
 app.listen(port, () => {
     console.log('http://localhost:5656')
