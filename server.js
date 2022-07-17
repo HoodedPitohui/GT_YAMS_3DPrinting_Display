ee0a8995ac1b8c88905dae3b90698cd5828d58bf
Karthik Shaji <58968966+HoodedPitohui@users.noreply.github.com>
2022-07-17 07:47:28 -0400

    initial pug work

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 5656;
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'pug');
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('index')
 })
-app.listen(5656, () => {
+app.get('/charles', (req, res) => {
+  res.render('index', {
+    firstname: 'Charles',
+    lastname: 'Chaplin',
+    tag: 'The Little Tramp',
+    content: '...',
+    movies: 'hi'
+  })
+})
+app.get('/marilyn', (req, res) => {
+  res.render('index', {
+    firstname: 'Marilyn',
+    lastname: 'Monroe',
+    tag: 'Being normal is boring',
+    content: '...',
+    movies: 'hi'
+  })
+})
+app.get('/jean', (req, res) => {
+  res.render('index')
+})
+app.get('/', (req, res) => {
+  res.redirect('/charles')
+})
+app.listen(port, () => {
     console.log('http://localhost:5656')
 })
